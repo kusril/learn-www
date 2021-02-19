d2deb5ef8ec6931a890586506ca777c5c2165549
Kusril Ubuntu <kusr1l@gmail.com>
2021-02-19 10:01:24 +0700

    initial commit from ubuntu

---
+++ LearningPHP.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Belajar PHP</title>
+    </head>
+<body>
+    <?php        
+        for($i=1;$i<=6;$i++){
+            echo"<h$i>This is H$i</h$i>";
+        }
+    ?>
+</body>
+</html>
---
+++ Welcome.php
@@ -0,0 +1,17 @@
+<?php
+echo("Hallo\tDunia....\n");
+
+/*Untuk mengetahui tipe data gunakan var_dump*/
+var_dump(12345);
+
+/*Here doc untuk membuat string kompleks*/
+echo <<<TEXT
+Ini adalah string yang dibuat dengan Heredoc.
+Dengan Heredoc string yang banyak bisa ditampilkan
+bisa, dengan "QUOTE"
+TEXT;
+
+$variabel = "Kusril Rasul";
+echo $variabel;
+
+?>
